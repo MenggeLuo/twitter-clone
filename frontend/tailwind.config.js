e45c50d4c1fc235926cf99977766a5ab6825f058
Your Name <your_email@example.com>
2024-08-17 19:59:58 -0400

    2- complete UI design

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+import daisyUIThemes from "daisyui/src/theming/themes";
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')], 
+  daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(29, 155, 240)",
+					secondary: "rgb(24, 24, 24)",
+				},
+			},
+		],
+	},
+}
