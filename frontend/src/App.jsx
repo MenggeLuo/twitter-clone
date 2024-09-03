fe1eef2cff3e8247a843ba837f1834b3690392a6
Your Name <your_email@example.com>
2024-09-02 23:49:38 -0400

    3- fetch profile page posts and user info

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,59 +1,63 @@
 import './output.css'
-import React, { useState } from 'react'
-//import './index.css'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import HomePage from './pages/home/HomePage'
-import SignUpPage from './pages/auth/signup/SignUpPage'
-import LoginPage from './pages/auth/login/LoginPage'
-import NotificationPage from './pages/notification/NotificationPage'
-import ProfilePage from './pages/profile/ProfilePage'
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import Sidbar from './components/common/Sidebar'
-import RightPanel from './components/common/RightPanel'
-import { Toaster } from 'react-hot-toast'
-import { useQuery } from '@tanstack/react-query'
-import LoadingSpinner from './components/common/LoadingSpinner'
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
-  const { data:authUser, isLoading } = useQuery({
-    queryKey: ['authUser'],
-    queryFn: async () => {
-      try {
-        const res = await fetch('/api/auth/me')
-        const data = await res.json()
-        if (data.error) return null
-        if (!res.ok) throw new Error(data.error || 'Fail to fetch user')
-        return data
-      } catch (error) {
-        throw new Error(error)
-      }
-    },
-    retry: false  
-  })
-  if (isLoading) {
-    return <div className='h-screen flex justify-center items-center'>
-      <LoadingSpinner size='large' />
-    </div>
-  }
-  
- 
+	const { data: authUser, isLoading } = useQuery({
+		// we use queryKey to give a unique name to our query and refer to it later
+		queryKey: ["authUser"],
+		queryFn: async () => {
+			try {
+				const res = await fetch("/api/auth/me");
+				const data = await res.json();
+				if (data.error) return null;
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				console.log("authUser is here:", data);
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		retry: false,
+	});
 
-  return (
-    <div className='flex max-w-6xl mx-auto'>
-      {/* Common components */}
-      {authUser && <Sidbar />}
-      <Routes>
-        <Route path='/' element={authUser ? <HomePage /> : <Navigate to = '/login' />} />
-        <Route path='/signup' element={!authUser ?<SignUpPage /> : <Navigate to = '/' />} />
-        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to = '/' />} />
-        <Route path='/notifications' element={!authUser ? <NotificationPage /> : <Navigate to = '/' /> } />
-        <Route path='/profile/:username' element={!authUser ? <ProfilePage /> : <Navigate to = '/' />} />
-      </Routes>
-      {authUser && <RightPanel />}
-      <Toaster />
-    </div>
+	if (isLoading) {
+		return (
+			<div className='h-screen flex justify-center items-center'>
+				<LoadingSpinner size='lg' />
+			</div>
+		);
+	}
 
-  );
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			{/* Common component, bc it's not wrapped with Routes */}
+			{authUser && <Sidebar />}
+			<Routes>
+				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
+				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+			</Routes>
+			{authUser && <RightPanel />}
+			<Toaster />
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,30 +1,42 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { useQuery } from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({feedType}) => {
-
+const Posts = ({ feedType, username, userId }) => {
 	const getPostEndpoint = () => {
 		switch (feedType) {
 			case "forYou":
 				return "/api/posts/all";
 			case "following":
 				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`;
 			default:
 				return "/api/posts/all";
 		}
 	};
 
-	const POST_ENDPOINT = getPostEndpoint(); 
+	const POST_ENDPOINT = getPostEndpoint();
 
-	const { data:posts, isLoading, refetch, isRefetching }= useQuery({
-		queryKey: ["posts", POST_ENDPOINT],
+	const {
+		data: posts,
+		isLoading,
+		refetch,
+		isRefetching,
+	} = useQuery({
+		queryKey: ["posts"],
 		queryFn: async () => {
 			try {
 				const res = await fetch(POST_ENDPOINT);
 				const data = await res.json();
-				if (!res.ok) throw new Error(data.error || "Failed to fetch posts");
+
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+
 				return data;
 			} catch (error) {
 				throw new Error(error);
@@ -34,7 +46,7 @@ const Posts = ({feedType}) => {
 
 	useEffect(() => {
 		refetch();
-	}, [feedType, refetch]);
+	}, [feedType, refetch, username]);
 
 	return (
 		<>
@@ -45,7 +57,9 @@ const Posts = ({feedType}) => {
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && !isRefetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && !isRefetching && posts?.length === 0 && (
+				<p className='text-center my-4'>No posts in this tab. Switch 👻</p>
+			)}
 			{!isLoading && !isRefetching && posts && (
 				<div>
 					{posts.map((post) => (
